4ef994fe2a588d69b64c4ec570e956d9cc835c69
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-02 00:49:47 +0600

    learn and practice about loop more

---
+++ looping.js
@@ -0,0 +1,38 @@
+/* 
+*-----Looping Technique---
+*for loop
+*while loop
+*do while loop
+*for of--> to access array
+*for in---> to access object
+*/
+
+
+const friends = ['Elon', 'Bill', 'Mark', 'Waren'];
+const numbers = [10, 23, 3, 5, 78, 96, 98, 546, 543, 198];
+
+/* for(let friend of friends){
+    console.log(friend);
+} */
+
+/* for(let i = 0; i < friends.length; i++){
+    let friend = friends[i];
+    console.log(friend);
+} */
+
+/* for(let n = 0; n < numbers.length; n++){
+    console.log(numbers[n]);
+} */
+
+// by using while loop
+/* let i = 0;
+while (i < friends.length) {
+    console.log(friends[i]);
+    i++;   
+} */
+
+/* let n = 0;
+while(n < numbers.length){
+    console.log(numbers[n]);
+    n++;
+} */
