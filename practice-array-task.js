11197aeef9c80bbce883e3a41a869fe3b1539a07
tofayeLL <ahmedtofayel132@gmail.com>
2024-02-02 11:42:28 +0600

    finished practice task about array task one, two, three

---
+++ practice-array-task.js
@@ -0,0 +1,45 @@
+
+/* ----practice task one---- */
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// const reverse = [];
+/* for(i = 0; i < colors.length; i++){
+    let color = colors[i];
+    reverse.unshift(color);
+}
+console.log(reverse); */
+
+// another way
+/* for(let color of colors){
+    // console.log(color);
+    reverse.unshift(color);
+}
+console.log(reverse); */
+
+// another way
+/* for(let i = colors.length-1; i >= 0; i--){
+    // console.log(colors[i]);
+    let color = colors[i];
+    reverse.push(color);
+
+}
+console.log(reverse); */
+
+
+/* -----practice task two---- */
+/* const numbers = [12, 98, 5, 41, 23, 78, 46];
+const even = []
+for(let i = 0; i < numbers.length; i++){
+    let num = numbers[i];
+    if(num % 2 === 0){
+        even.push(num);
+    }
+}
+console.log(even); */
+
+
+/* ----practice task three------ */
+/* var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+var str = numbers.join('');
+console.log(str);
+ */
+
